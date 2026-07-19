9963fabe6fa14e8751213f13549eabfc427cd02e
diceworks <diceysnikey@gmail.com>
2026-07-19 22:06:00 +0200

    Changed the tutorial start to show a mouse. Sprite is traced over reference by Nicolae (Xelu) Berbece

--- scripts/tutorial_director.gd
+++ scripts/tutorial_director.gd
@@ -2,21 +2,28 @@ extends Node2D
 
 var Spawner:Node2D
 
-func _show_move_keys() -> void:
-	$UpIcon.visible = true
-	$DownIcon.visible = true
-	$FullscreenIcon.visible = true
-	await get_tree().create_timer(4.5).timeout
-	$UpIcon.visible = false
-	$DownIcon.visible = false
-	$FullscreenIcon.visible = false
+func _show_mouse_help_tween() -> void:
+	await get_tree().create_timer(1).timeout
+	var _mouse_tween_visiblity_true = get_tree().create_tween().bind_node(self).tween_property(
+		$sprite_mouse, "modulate:a", 1, 1).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+	var mouse_tween_movement = get_tree(
+	).create_tween().bind_node(self).set_ease(
+		Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).set_loops(3)
+	mouse_tween_movement.tween_property(
+		$sprite_mouse, "position:y", 200, 1).as_relative()
+	mouse_tween_movement.chain().tween_property(
+		$sprite_mouse, "position:y", -200, 1).as_relative()
+	await get_tree().create_timer(5).timeout #second to last loop of movement tween
+	var _mouse_tween_visiblity_false = get_tree().create_tween().bind_node(self).tween_property(
+		$sprite_mouse, "modulate:a", 0, 1).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+	await get_tree().create_timer(2.5).timeout
 	
-func _collect_asteroid() -> void:
-	$CollectText.visible = true
+func _catch_asteroid() -> void:
+	$CatchText.visible = true
 	Spawner._spawn_asteroid()
 
 func _dodge_asteroid() -> void:
-	$CollectText.visible = false
+	$CatchText.visible = false
 	await get_tree().create_timer(1).timeout
 	$DodgeText.visible = true
 	Spawner._spawn_bad_asteroid()
@@ -41,9 +48,9 @@ func _ready() -> void:
 	Director.in_tutorial_mode = true
 	Director._spawn_spawner()
 	Spawner = get_node("/root/Director/Spawner")
-	await _show_move_keys()
-	_collect_asteroid()
-	Signalbus.tutorial_asteroid_missed.connect(_collect_asteroid)
+	await _show_mouse_help_tween()
+	_catch_asteroid()
+	Signalbus.tutorial_asteroid_missed.connect(_catch_asteroid)
 	Signalbus.tutorial_asteroid_collected.connect(_dodge_asteroid)
 	Signalbus.tutorial_badAsteroid_dodged.connect(_dodge_success)
 	Signalbus.tutorial_hit_by_badAsteroid.connect(_dodge_failed)
